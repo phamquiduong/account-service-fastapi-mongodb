afff00a90354ca24cdddd88bf3302f03d0001e85
phamquiduong <phamquiduong@outlook.com>
2026-05-03 02:19:33 +0700

    rename repository to manager

--- src/services/user.py
+++ src/services/user.py
@@ -12,25 +12,25 @@ _logger = logging.getLogger(__name__)
 
 class UserService:
     def __init__(self, user_mongo_manager: BaseMongoManager[User]):
-        self.user_repository = user_mongo_manager
+        self.user_mongo_manager = user_mongo_manager
 
     async def get_by_id(self, user_id: uuid.UUID) -> User | None:
-        return await self.user_repository.get_by_id(id_value=user_id)
+        return await self.user_mongo_manager.get_by_id(id_value=user_id)
 
     async def get_by_email(self, email: EmailStr) -> User | None:
-        return await self.user_repository.get(query={"email": email})
+        return await self.user_mongo_manager.get(query={"email": email})
 
     async def update_password(self, user_id: uuid.UUID, new_password: str):
         new_password_hashed = get_password_hash(new_password)
-        await self.user_repository.update_by_id(user_id, {"password": new_password_hashed})
+        await self.user_mongo_manager.update_by_id(user_id, {"password": new_password_hashed})
 
     async def list(self, skip: int, limit: int) -> list[User]:
-        return await self.user_repository.list(skip=skip, limit=limit)
+        return await self.user_mongo_manager.list(skip=skip, limit=limit)
 
     async def create(self, email: EmailStr, password: str) -> User:
         hashed_password = get_password_hash(password)
         user = User(email=email, password=hashed_password)
-        await self.user_repository.create(user)
+        await self.user_mongo_manager.create(user)
         return user
 
     async def authenticate_user(self, email: EmailStr, password: str) -> User | None:
